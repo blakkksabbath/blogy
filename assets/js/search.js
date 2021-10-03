af6be698e708bbb7738b331b596a03b9d32bedc8
blaksab <blaksab@users.noreply.github.com>
2021-10-03 06:39:40 +0000

    deploy: 8fd0d7570e7a2300703965c30444154ea76f2dab

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/blogy/assets/js/search-data.json', true);
+    request.open('GET', '/blogys/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
